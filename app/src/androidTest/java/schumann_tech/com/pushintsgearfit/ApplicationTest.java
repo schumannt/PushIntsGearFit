77b890b0d23bb1d14ac803315b1e79cff02e7742
Terry Schumann <terry.schumann@uk.tesco.com>
2015-02-25 20:22:42 +0000

    Initial commit

---
+++ app/src/androidTest/java/schumann_tech/com/pushintsgearfit/ApplicationTest.java
@@ -0,0 +1,13 @@
+package schumann_tech.com.pushintsgearfit;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
